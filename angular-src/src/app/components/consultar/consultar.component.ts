6fde05d6d86af9374b6d3c38d6919b0e36d7727b
carlotach <carlotacanamero@gmail.com>
2021-04-15 16:07:38 +0200

    Consultar y Eliminar acabadas

--- angular-src/src/app/components/consultar/consultar.component.ts
+++ angular-src/src/app/components/consultar/consultar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConsultarService } from '../../services/consultar.service';
+import { HttpClient } from '@angular/common/http';
+import  Swal  from 'sweetalert2';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-consultar',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private consultarService: ConsultarService) { }
+  productos: any = [];
+  filtro: String;
+
 
   ngOnInit(): void {
+
+    this.getListaProductos();
+
   }
+
+  getListaProductos() {
+    this.consultarService.getLista().subscribe((productos) => { 
+      (this.productos=productos.msg);
+    },
+      (err=>console.log('Error al listar', err)));
+    
+
+  }
+
+
+  deleteProducto(id: String){
+    
+    this.consultarService.borrarProducto(id).subscribe((data) => {
+      console.log("data: "+data);
+      if(data.success){
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Producto eliminado',
+          showConfirmButton: false,
+          timer: 1500
+        }).then(()=>{
+          location.reload();
+        });
+      }else{
+        Swal.fire({
+          icon: 'error',
+          text: 'Ha ocurrido un error',
+
+        })
+      }
+    });
+
+
+  }
+
+
+  buscarProducto(cadena: String){
+    this.consultarService.filtrarProductos(cadena).subscribe();
+  }
+
+
 
 }
---
+++ angular-src/src/app/services/consultar.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Producto } from '../../../../models/producto';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsultarService {
+
+  private URL = "http://localhost:9000/admin/consultar";
+
+  // headers: HttpHeaders = new HttpHeaders({
+  //  'Content-Type': 'application/json'
+  // });
+
+  constructor(private http:HttpClient) { }
+
+  getLista(): Observable<any>{
+
+    return this.http.get(this.URL);
+
+  }
+
+  borrarProducto(id: String) : Observable<any> {
+    return this.http.delete(this.URL= this.URL+"/"+id).pipe(map(id=>id));
+  }
+
+  filtrarProductos(cadena: String){
+    return this.http.get(this.URL= this.URL+"/"+cadena).pipe(map((id=>id) || (nombre=>nombre)));
+  }
+  
+}
